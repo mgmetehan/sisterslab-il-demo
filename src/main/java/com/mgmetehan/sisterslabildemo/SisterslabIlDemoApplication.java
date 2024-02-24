b426cb409fe99c21ac77ff99d94c47c0d6fc9b82
mgmetehan <mgmetehan@hotmail.com>
2024-02-24 09:41:03 +0300

    init project

---
+++ src/main/java/com/mgmetehan/sisterslabildemo/SisterslabIlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.sisterslabildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisterslabIlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisterslabIlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgmetehan/sisterslabildemo/SisterslabIlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.sisterslabildemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisterslabIlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
